e96b8521f185122befd130b724a83366f97ed92b
Lena Konstantinova <smile711@mail.ru>
2022-06-29 12:26:01 +0300

    Added a solution of task 'fp'

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -1,15 +1,16 @@
+require 'csv'
 module Exercise
   module Fp
     class << self
-      # Обратиться к параметрам фильма можно так:
-      # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
-      # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        array1 = array.map { |item| item if item[6].to_s.include? ',' }.delete_if(&:nil?)
+        array1.map! { |item| item if item[1].to_s != '0' && !item[1].nil? }.delete_if(&:nil?)
+        array1.reduce(0) { |count, item| count + item[1].to_f } / array1.size
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(array, threshold)
+        array1 = array.map { |item| item if item[1].to_f >= threshold }.delete_if(&:nil?)
+        array1.reduce(0) { |count, item| count + item[0].count('и') }
       end
     end
   end
