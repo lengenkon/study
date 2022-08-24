909226c5cf64121c0ae29cbe3c35b6a8fcc9b02c
Lena Konstantinova <smile711@mail.ru>
2022-08-24 14:35:14 +0300

    2.Corrected solution of task 'arrays'

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,30 +2,21 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        max = array[0]
-        my_max = ->(item) { max = item if item > max }
-        array.each(&my_max)
-        array.map! { |item| item.positive? ? max : item }
-        array
+        def my_max(array)
+          max = array[0]
+          array.each { |item| max = item if item > max }
+          max
+        end
+        array.map! { |item| item.positive? ? my_max(array) : item }
       end
 
-      def search(array, query, first = nil, last = nil)
-        if first.nil? && last.nil?
-          first = 0
-          last = array.size - 1
-        end
-        if first > last
-          -1
-        else
-          middle = (last + first) / 2
-          if query == array[middle]
-            middle
-          elsif array[middle] < query
-            search(array, query, middle + 1, last)
-          else
-            search(array, query, first, middle - 1)
-          end
-        end
+      def search(array, query, first = 0, last = array.size - 1)
+        return -1 if first > last && query > last
+
+        middle = (last + first) / 2
+        return middle if query == array[middle]
+
+        array[middle] < query ? search(array, query, middle + 1, last) : search(array, query, first, middle - 1)
       end
     end
   end
