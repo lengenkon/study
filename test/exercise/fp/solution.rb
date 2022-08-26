a157f5c1b85cb707c68b678e99ad3c1c1624b151
Lena Konstantinova <smile711@mail.ru>
2022-08-26 21:20:54 +0300

    Created a boolean method. Renamed variables

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -2,15 +2,20 @@ require 'csv'
 module Exercise
   module Fp
     class << self
+      def film_with_several_countries_and_with_rating_kinopoisk?(film)
+        (film['country'].to_s.include? ',') && (film['rating_kinopoisk'].to_s != '0' && !film['rating_kinopoisk'].nil?)
+      end
+
       def rating(array)
-        array1 = array.map { |item| item if item['country'].to_s.include? ',' }.compact!
-        array1.map! { |item| item if item['rating_kinopoisk'].to_s != '0' && !item['rating_kinopoisk'].nil? }.compact!
-        array1.reduce(0) { |count, item| count + item['rating_kinopoisk'].to_f } / array1.size
+        selected_films = array.select { |item| film_with_several_countries_and_with_rating_kinopoisk?(item) }
+        selected_films.reduce(0) do |count, item|
+          count + item['rating_kinopoisk'].to_f
+        end / selected_films.size
       end
 
       def chars_count(array, threshold)
-        array1 = array.map { |item| item if item['rating_kinopoisk'].to_f >= threshold }.compact!
-        array1.reduce(0) { |sum, item| sum + item['name'].count('и') }
+        films_with_rating_more_thershold = array.select { |item| item if item['rating_kinopoisk'].to_f >= threshold }
+        films_with_rating_more_thershold.reduce(0) { |sum, item| sum + item['name'].count('и') }
       end
     end
   end
