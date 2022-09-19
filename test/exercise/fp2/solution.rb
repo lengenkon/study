7f0d287b267da945c9001c6ccb1823567654daa6
Lena Konstantinova <smile711@mail.ru>
2022-09-19 19:04:19 +0300

    1.Corrected solution. Recursive method my_each

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -1,34 +1,38 @@
 module Exercise
   module Fp2
     class MyArray < Array
-      def my_each
-        each { |element| yield element }
+      def my_each(array = self, &block)
+        return MyArray.new(array) if empty?
+
+        first, *rest = self
+        block.call(first)
+        MyArray.new(rest).my_each(array, &block)
       end
 
-      def my_map
-        reduce(MyArray.new) { |acc, element| acc << (yield element) }
-      end
-
-      def my_compact
-        reduce(MyArray.new) do |acc, element|
-          acc << element unless element.is_a? NilClass
-          acc
-        end
-      end
-
-      def my_reduce(acc = nil, &block)
-        if acc.nil?
+      def my_reduce(accumulator = nil, &block)
+        if accumulator.nil?
           ignore_first = true
-          acc = self[0]
+          accumulator = self[0]
         end
 
         index = 0
 
-        each do |element|
-          acc = block.call(acc, element) unless ignore_first && index.zero?
+        my_each do |element|
+          accumulator = block.call(accumulator, element) unless ignore_first && index.zero?
           index += 1
         end
-        acc
+        accumulator
+      end
+
+      def my_map
+        my_reduce(MyArray.new) { |acc, element| acc << (yield element) }
+      end
+
+      def my_compact
+        my_reduce(MyArray.new) do |acc, element|
+          acc << element unless element.is_a? NilClass
+          acc
+        end
       end
     end
   end
